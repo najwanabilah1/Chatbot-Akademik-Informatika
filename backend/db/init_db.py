4e792cb7952093317e262a778cb1a8bea11f60b7
najwanabilah1 <najwanaa16@gmail.com>
2025-12-17 19:43:49 +0700

    Delete backend directory

--- backend/db/init_db.py
+++
@@ -1,6 +0,0 @@
-from backend.config import db, app
-from backend.db.models import User, ChatHistory, Dataset, Peraturan, WebsiteStats
-
-with app.app_context():
-    db.create_all()
-    print("✅ Database & tabel berhasil dibuat!")
